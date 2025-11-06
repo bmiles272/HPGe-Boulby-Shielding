c78c963e2aa4e895c5d578514baf7519cf47e225
Bram Miles <brammiles@gmail.com>
2025-11-06 09:53:35 +0000

    changes in geometry of hpge and cavity for layers

--- src/detectorShielding.cc
+++ src/detectorShielding.cc
@@ -17,20 +17,18 @@
 
 detectorShielding::detectorShielding()
  : G4VUserDetectorConstruction(),
-   fHPGeHalfX(2.5*cm),
-   fHPGeHalfY(2.5*cm),
-   fHPGeHalfZ(3.0*cm),
-   fInnerCu1Thickness(0.5*cm),
-   fInnerCu2Thickness(2.0*cm),
-   fOuterPb1Thickness(5.0*cm),
-   fOuterPb2Thickness(15.0*cm),
+   fHPGeHalfX(115*mm),
+   fHPGeHalfY(225*mm),
+   fHPGeHalfZ(115*mm),
+   fInnerCu1Thickness(5*mm),
+   fInnerCu2Thickness(20*mm),
+   fOuterPb1Thickness(50*mm),
+   fOuterPb2Thickness(150*mm),
+   fCavityHalfX(50*mm),
+   fCavityHalfY(10*mm),
+   fCavityHalfZ(60*mm),
    fMessenger(nullptr)
 {
-  // compute cavity half-sizes as conservative default (can be adjusted via messenger)
-  fCavityHalfX = fHPGeHalfX + fInnerCu1Thickness + fInnerCu2Thickness + fOuterPb1Thickness + fOuterPb2Thickness;
-  fCavityHalfY = fCavityHalfX;
-  fCavityHalfZ = fCavityHalfX;
-
   // optional: create simple messenger for runtime control (safe if header included)
   fMessenger = new G4GenericMessenger(this, "/geometry/", "Geometry control");
   // You can add commands here if desired, e.g. to change thicknesses before Construct is called.
@@ -76,62 +74,67 @@ G4VPhysicalVolume* detectorShielding::DefineVolumes()
   auto logicHPGe = new G4LogicalVolume(solidHPGe, Ge, "HPGe");
   new G4PVPlacement(nullptr, {}, logicHPGe, "HPGe", logicWorld, false, 0, true);
 
-  // ---------------- Shielding layers as hollow shells ----------------
-  //
-  // Inner copper shell (Cu1)
+  // ---------------- Shielding layers ----------------
+  G4double gap = 50*mm;
+
+  // compute cumulative half-sizes (measured from center)
+  G4double hpge_hx = fHPGeHalfX;
+
+  // First calculate the maximum extent of HPGe + cavity in any direction
+  G4double maxInnerExtentX = fHPGeHalfX + fCavityHalfX;
+  G4double maxInnerExtentY = fHPGeHalfY + fCavityHalfY;
+  G4double maxInnerExtentZ = fHPGeHalfZ + fCavityHalfZ;
+  
+  // Use the largest dimension to ensure cubic shells fully contain detector+cavity
+  G4double maxInnerExtent = std::max({maxInnerExtentX, maxInnerExtentY, maxInnerExtentZ});
+  
+  // Now define cubic shells using this maximum extent
+  G4double cu1_inner_half = maxInnerExtent;
+  G4double cu1_outer_half = cu1_inner_half + fInnerCu1Thickness;
+
+  G4double cu2_inner_half = cu1_outer_half;
+  G4double cu2_outer_half = cu2_inner_half + fInnerCu2Thickness;
+
+  G4double pb1_inner_half = cu2_outer_half;
+  G4double pb1_outer_half = pb1_inner_half + fOuterPb1Thickness;
+
+  G4double pb2_inner_half = pb1_outer_half;
+  G4double pb2_outer_half = pb2_inner_half + fOuterPb2Thickness;
+
   G4ThreeVector origin = {0, 0, 0};
+
+  // Inner copper shell (Cu1) using the computed halves
   auto solidCu1_outer = new G4Box("Cu1_outer",
-      fHPGeHalfX + fInnerCu1Thickness,
-      fHPGeHalfY + fInnerCu1Thickness,
-      fHPGeHalfZ + fInnerCu1Thickness);
-
-  auto solidCu1_inner = new G4Box("Cu1_inner", fHPGeHalfX, fHPGeHalfY, fHPGeHalfZ);
+      cu1_outer_half, cu1_outer_half, cu1_outer_half);
+  auto solidCu1_inner = new G4Box("Cu1_inner",
+      cu1_inner_half, cu1_inner_half, cu1_inner_half);
   auto solidCu1 = new G4SubtractionSolid("Cu1", solidCu1_outer, solidCu1_inner);
-
   auto logicCu1 = new G4LogicalVolume(solidCu1, Cu1Material, "Cu1");
   new G4PVPlacement(nullptr, origin, logicCu1, "Cu1", logicWorld, false, 0, true);
 
   // Outer copper shell (Cu2)
   auto solidCu2_outer = new G4Box("Cu2_outer",
-      fHPGeHalfX + fInnerCu1Thickness + fInnerCu2Thickness,
-      fHPGeHalfY + fInnerCu1Thickness + fInnerCu2Thickness,
-      fHPGeHalfZ + fInnerCu1Thickness + fInnerCu2Thickness);
-
+      cu2_outer_half, cu2_outer_half, cu2_outer_half);
   auto solidCu2_inner = new G4Box("Cu2_inner",
-      fHPGeHalfX + fInnerCu1Thickness,
-      fHPGeHalfY + fInnerCu1Thickness,
-      fHPGeHalfZ + fInnerCu1Thickness);
-
+      cu2_inner_half, cu2_inner_half, cu2_inner_half);
   auto solidCu2 = new G4SubtractionSolid("Cu2", solidCu2_outer, solidCu2_inner);
   auto logicCu2 = new G4LogicalVolume(solidCu2, Cu2Material, "Cu2");
   new G4PVPlacement(nullptr, origin, logicCu2, "Cu2", logicWorld, false, 0, true);
 
   // Inner lead shell (Pb1)
   auto solidPb1_outer = new G4Box("Pb1_outer",
-      fHPGeHalfX + fInnerCu1Thickness + fInnerCu2Thickness + fOuterPb1Thickness,
-      fHPGeHalfY + fInnerCu1Thickness + fInnerCu2Thickness + fOuterPb1Thickness,
-      fHPGeHalfZ + fInnerCu1Thickness + fInnerCu2Thickness + fOuterPb1Thickness);
-
+      pb1_outer_half, pb1_outer_half, pb1_outer_half);
   auto solidPb1_inner = new G4Box("Pb1_inner",
-      fHPGeHalfX + fInnerCu1Thickness + fInnerCu2Thickness,
-      fHPGeHalfY + fInnerCu1Thickness + fInnerCu2Thickness,
-      fHPGeHalfZ + fInnerCu1Thickness + fInnerCu2Thickness);
-
+      pb1_inner_half, pb1_inner_half, pb1_inner_half);
   auto solidPb1 = new G4SubtractionSolid("Pb1", solidPb1_outer, solidPb1_inner);
   auto logicPb1 = new G4LogicalVolume(solidPb1, Pb1Material, "Pb1");
   new G4PVPlacement(nullptr, origin, logicPb1, "Pb1", logicWorld, false, 0, true);
 
   // Outer lead shell (Pb2)
   auto solidPb2_outer = new G4Box("Pb2_outer",
-      fHPGeHalfX + fInnerCu1Thickness + fInnerCu2Thickness + fOuterPb1Thickness + fOuterPb2Thickness,
-      fHPGeHalfY + fInnerCu1Thickness + fInnerCu2Thickness + fOuterPb1Thickness + fOuterPb2Thickness,
-      fHPGeHalfZ + fInnerCu1Thickness + fInnerCu2Thickness + fOuterPb1Thickness + fOuterPb2Thickness);
-
+      pb2_outer_half, pb2_outer_half, pb2_outer_half);
   auto solidPb2_inner = new G4Box("Pb2_inner",
-      fHPGeHalfX + fInnerCu1Thickness + fInnerCu2Thickness + fOuterPb1Thickness,
-      fHPGeHalfY + fInnerCu1Thickness + fInnerCu2Thickness + fOuterPb1Thickness,
-      fHPGeHalfZ + fInnerCu1Thickness + fInnerCu2Thickness + fOuterPb1Thickness);
-
+      pb2_inner_half, pb2_inner_half, pb2_inner_half);
   auto solidPb2 = new G4SubtractionSolid("Pb2", solidPb2_outer, solidPb2_inner);
   auto logicPb2 = new G4LogicalVolume(solidPb2, Pb2Material, "Pb2");
   new G4PVPlacement(nullptr, origin, logicPb2, "Pb2", logicWorld, false, 0, true);
@@ -143,19 +146,19 @@ G4VPhysicalVolume* detectorShielding::DefineVolumes()
   hpgeVis->SetForceSolid(true);
   logicHPGe->SetVisAttributes(hpgeVis);
 
-  auto cu1Vis = new G4VisAttributes(G4Colour(0.8, 0.4, 0.1, 0.3));
+  auto cu1Vis = new G4VisAttributes(G4Colour(0.8, 0.4, 0.1, 0.25));
   cu1Vis->SetForceSolid(true);
   logicCu1->SetVisAttributes(cu1Vis);
 
-  auto cu2Vis = new G4VisAttributes(G4Colour(0.9, 0.5, 0.1, 0.3));
+  auto cu2Vis = new G4VisAttributes(G4Colour(0.9, 0.5, 0.1, 0.25));
   cu2Vis->SetForceSolid(true);
   logicCu2->SetVisAttributes(cu2Vis);
 
-  auto pb1Vis = new G4VisAttributes(G4Colour(0.4, 0.4, 0.4, 0.3));
+  auto pb1Vis = new G4VisAttributes(G4Colour(0.4, 0.4, 0.4, 0.2));
   pb1Vis->SetForceSolid(true);
   logicPb1->SetVisAttributes(pb1Vis);
 
-  auto pb2Vis = new G4VisAttributes(G4Colour(0.2, 0.2, 0.2, 0.3));
+  auto pb2Vis = new G4VisAttributes(G4Colour(0.2, 0.2, 0.2, 0.15));
   pb2Vis->SetForceSolid(true);
   logicPb2->SetVisAttributes(pb2Vis);
 
